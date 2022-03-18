ab1f068d2f179f28fcae3c52500abf1b198d214e
TrevorRezac <trev.rezac@gmail.com>
2022-03-18 14:44:38 -0700

    set up movies table, wrote passing test for movies post route

--- __tests__/movies.test.js
+++ __tests__/movies.test.js
@@ -11,4 +11,19 @@ describe('hand-of-resources routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+  it('should create a new movie', async () => {
+    const res = await request(app).post('/api/v1/movies').send({
+      title: 'Pulp Fiction',
+      released: 1994,
+      genre: 'Crime/Drama',
+    });
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      title: 'Pulp Fiction',
+      released: 1994,
+      genre: 'Crime/Drama',
+    });
+  });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS cars;
+DROP TABLE IF EXISTS cars, movies;
+-- DROP TABLE IF EXISTS movies;
 
 CREATE TABLE cars (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -8,4 +9,12 @@ CREATE TABLE cars (
   make TEXT NOT NULL,
   model TEXT NOT NULL
 );
+
+CREATE TABLE movies (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title TEXT NOT NULL,
+  released INT,
+  genre TEXT NOT NULL
+);
+
 
