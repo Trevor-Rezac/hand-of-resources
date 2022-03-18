63e387a89885be4a549ef6752baf9cb8bdbda4c5
TrevorRezac <trev.rezac@gmail.com>
2022-03-17 19:34:04 -0700

    hard coded passing TDD for cars get all route

--- __tests__/cars.test.js
+++ __tests__/cars.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Car = require('../lib/models/Car');
 
 describe('hand-of-resources routes', () => {
   beforeEach(() => {
@@ -25,6 +26,23 @@ describe('hand-of-resources routes', () => {
       year: 1969,
       make: 'Ford',
       model: 'Mustang',
+    });
+  });
+
+  it('should list all cars', async () => {
+    await Car.create({
+      year: 1984,
+      make: 'Honda',
+      model: 'Accord',
+    });
+
+    const res = await request(app).get('/api/v1/cars');
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      year: 1984,
+      make: 'Honda',
+      model: 'Accord',
     });
   });
 });
