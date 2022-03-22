806473bafe86f12efd0538405b2c8f136e8e4494
TrevorRezac <trev.rezac@gmail.com>
2022-03-21 17:10:20 -0700

    hard coded passing test for get all route

--- __tests__/shows.test.js
+++ __tests__/shows.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Show = require('../lib/models/Show');
 
 describe('hand-of-resources routes', () => {
   beforeEach(() => {
@@ -27,6 +28,23 @@ describe('hand-of-resources routes', () => {
       title: 'Rick and Morty',
       seasons: 5,
       network: 'Adult Swim',
+    });
+  });
+
+  it('should list all shows', async () => {
+    await Show.insert({
+      title: 'show title',
+      seasons: 4,
+      network: 'show network',
+    });
+
+    const res = await request(app).get('/api/v1/shows');
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      title: 'show title',
+      seasons: 4,
+      network: 'show network',
     });
   });
 });
