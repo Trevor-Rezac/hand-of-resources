87b186c0b0a72ce2ad89a4d017b7f633d2411d63
TrevorRezac <trev.rezac@gmail.com>
2022-03-22 09:33:36 -0700

    hard coded passing test for band controller post route

--- __tests__/bands.test.js
+++ __tests__/bands.test.js
@@ -11,4 +11,21 @@ describe('hand-of-resources routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+  it('should insert a new band', async () => {
+    const band = {
+      name: 'Red Hot Chili Peppers',
+      genre: 'Rock',
+      albums: 12,
+    };
+
+    const res = await request(app).post('/api/v1/bands').send(band);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      name: 'Red Hot Chili Peppers',
+      genre: 'Rock',
+      albums: 12,
+    });
+  });
 });
